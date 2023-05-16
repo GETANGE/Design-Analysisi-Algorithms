948d9fd47a821b168608aaad3444f57a8471a9c7
Getange <emmanuelgetange48@gmail.com>
2023-05-16 09:48:23 +0300

    Added a folder

---
+++ Searching/search.js
@@ -0,0 +1,13 @@
+//serching in a sequence(unordered sequence)
+function Sequence(s,x){
+    for(var i=0;i<s.length;i++){
+        if(s[i]===x){
+            return i;//return the index of the element if it exists.
+        }
+    }
+    return false;//if the element doesn't exist.
+}
+const sequence = [3,8,2,1,5];
+const searchValue=2;
+const index=Sequence(sequence,searchValue)
+console.log("The index of the value searched is: "+index)
