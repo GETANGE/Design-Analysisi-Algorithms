c2ce0f99191017fd945345f37762f6eb46c7f2d1
Getange <emmanuelgetange48@gmail.com>
2023-05-18 00:06:14 +0300

    practice

---
+++ Searching/search2.js
@@ -0,0 +1,13 @@
+function Practice1(s,p){
+    for(let i=0;i<s.length;i++){
+        if(s[i]==p){
+            return i;
+        }
+    }
+    return false;
+}
+
+const sequence = [1,3,5,7,9]
+const value=9;
+const result=Practice1(sequence, value);
+console.log(result);
