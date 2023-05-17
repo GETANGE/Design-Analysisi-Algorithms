ef72e7301f3bf0815ddc853d877eb29d8e8b789a
Getange <emmanuelgetange48@gmail.com>
2023-05-18 00:00:18 +0300

    debugged

--- Searching/search.js
+++ Searching/search.js
@@ -1,6 +1,6 @@
 //serching in a sequence(unordered sequence)
 function Sequence(s,x){
-    for(var i=0;i<s.length;i++){
+    for(var i=0; i<s.length;i++){
         if(s[i]===x){
             return i;//return the index of the element if it exists.
         }
